96c2d90acf2e0de900b706ad79778f1f32f2c0ff
Levente Mathe <levente.l.mathe@gmail.com>
2020-01-29 10:37:34 +0100

    add mute button

--- assets/js/tik-video.js
+++ assets/js/tik-video.js
@@ -1,13 +1,46 @@
-const videoTag = document.querySelector('.tik');
+const video = document.querySelector('.tik');
+const videControls = document.querySelector('.videoOverlay')
 const playButton = document.querySelector('.videoOverlay');
+const muteButton = document.querySelector('.muteButton');
 
 playButton.addEventListener('click', function(event) {
   event.preventDefault();
-  if (videoTag.paused) {
-    videoTag.play();
+  if (video.paused) {
+    video.play();
     playButton.style.opacity = 0;
   } else {
-    videoTag.pause();
+    video.pause();
     playButton.style.opacity = 1;
   }
+})
+
+muteButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  if (muteButton.classList.contains('muteButtonDisableSound')) {
+    muteButton.classList.remove('muteButtonDisableSound');
+    muteButton.classList.add('muteButtonEnableSound');
+    video.muted = true;
+  } else {
+    muteButton.classList.remove('muteButtonEnableSound');
+    muteButton.classList.add('muteButtonDisableSound');
+    video.muted = false;
+  }
+})
+
+videControls.addEventListener('mouseenter', function(event) {
+  event.preventDefault();
+  muteButton.classList.remove('muteButtonHidden');
+  muteButton.classList.add('muteButtonShown');
+})
+
+muteButton.addEventListener('mouseenter', function(event) {
+  event.preventDefault();
+  muteButton.classList.remove('muteButtonHidden');
+  muteButton.classList.add('muteButtonShown');
+})
+
+videControls.addEventListener('mouseleave', function(event) {
+  event.preventDefault();
+  muteButton.classList.remove('muteButtonShown');
+  muteButton.classList.add('muteButtonHidden');
 })
